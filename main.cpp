a0d1e845aa9b594d8116528359808cf2dd03fd45
El_Rovi Soft <el.rovisoft@gmail.com>
2025-03-17 07:52:23 +0300

    removed redundant code from main

--- main.cpp
+++ main.cpp
@@ -5,18 +5,6 @@ int main() {
     backend::Backend server;
 
     server.init();
-    auto toml = lib::load_by_id("players", 1500438496);
-    auto input = global::to_eval_data_details.from(toml);
-    auto result = calculator::Calculator::eval(input);
-
-    auto toml = lib::load_by_id("players", 1500438496);
-    auto input = global::to_eval_data_details.from(toml);
-    auto result = calculator::Calculator::eval(input);
-
-    auto toml = lib::load_by_id("players", 1500438496);
-    auto input = global::to_eval_data_details.from(toml);
-    auto result = calculator::Calculator::eval(input);
-
     server.run();
 
     return 0;
